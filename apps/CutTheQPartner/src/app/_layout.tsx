b4b7117a3bd34161050c7168ac77c779cffe032d
iamnithishraja <153272518+iamnithishraja@users.noreply.github.com>
2024-10-07 00:52:46 +0530

    apps for user and partner

---
+++ apps/CutTheQPartner/src/app/_layout.tsx
@@ -0,0 +1,38 @@
+import {
+  DarkTheme,
+  DefaultTheme,
+  ThemeProvider,
+} from "@react-navigation/native";
+import { useFonts } from "expo-font";
+import * as SplashScreen from "expo-splash-screen";
+import { useEffect } from "react";
+import "react-native-reanimated";
+import { useColorScheme } from "@repo/hooks";
+import { Text, View } from "react-native";
+
+SplashScreen.preventAutoHideAsync();
+
+export default function RootLayout() {
+  const colorScheme = useColorScheme();
+  const [loaded] = useFonts({
+    SpaceMono: require("../../assets/fonts/SpaceMono-Regular.ttf"),
+  });
+
+  useEffect(() => {
+    if (loaded) {
+      SplashScreen.hideAsync();
+    }
+  }, [loaded]);
+
+  if (!loaded) {
+    return null;
+  }
+
+  return (
+    <ThemeProvider value={colorScheme === "dark" ? DarkTheme : DefaultTheme}>
+      <View style={{ flex: 1, alignItems: "center", justifyContent: "center" }}>
+        <Text>this is partner app</Text>
+      </View>
+    </ThemeProvider>
+  );
+}
