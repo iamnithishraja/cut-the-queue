6811768f8c908ed4b1f47c6e7d374db354acb9c1
Labbey Ammar Ahmed <141624572+ammar-codeable@users.noreply.github.com>
2024-11-03 18:24:21 +0530

    refactor: update files to match modified socket manager api

--- apps/socket/src/controllers/orderControllers.ts
+++ apps/socket/src/controllers/orderControllers.ts
@@ -1,40 +1,39 @@
-import { Request, Response } from "express";
-import prisma from "@repo/db/client";
 import {
-  DISHES_NOT_FOUND,
-  BROADCAST_QUANTITY,
-  SERVER_ERROR,
+	BROADCAST_QUANTITY,
+	DISHES_NOT_FOUND,
+	SERVER_ERROR,
 } from "@repo/constants";
-import socketManager from "../socketManager";
+import prisma from "@repo/db/client";
+import { Request, Response } from "express";
+import { getAllSockets } from "../socketManager";
 
 export const broadcastQuantity = async (
-  req: Request,
-  res: Response
+	req: Request,
+	res: Response
 ): Promise<any> => {
-  const canteenId = req.params.canteenId;
-  try {
-    const updatedMenuItems = await prisma.menuItem.findMany({
-      where: {
-        canteenId: canteenId,
-      },
-    });
+	const canteenId = req.params.canteenId;
+	try {
+		const updatedMenuItems = await prisma.menuItem.findMany({
+			where: {
+				canteenId: canteenId,
+			},
+		});
 
-    if (!updatedMenuItems || updatedMenuItems.length === 0) {
-      res.status(404).json({ message: DISHES_NOT_FOUND });
-      return;
-    }
-    const data = JSON.stringify({
-      type: "UPDATE_MENU_ITEMS",
-      payload: updatedMenuItems,
-    });
-    const socketInstance = socketManager.getInstance();
-    const connectedDevices = socketInstance.getAllSockets();
-    connectedDevices.forEach((device) => {
-      device.send(data);
-    });
-    res.status(200).json({ message: BROADCAST_QUANTITY });
-  } catch (e) {
-    console.error(e);
-    res.status(500).json({ message: SERVER_ERROR });
-  }
+		if (!updatedMenuItems || updatedMenuItems.length === 0) {
+			res.status(404).json({ message: DISHES_NOT_FOUND });
+			return;
+		}
+		const data = JSON.stringify({
+			type: "UPDATE_MENU_ITEMS",
+			payload: updatedMenuItems,
+		});
+		const connectedDevices = getAllSockets();
+		connectedDevices.forEach((device) => {
+			device.send(data);
+		});
+		res.status(200).json({ message: BROADCAST_QUANTITY });
+	} catch (e) {
+		console.error(e);
+		res.status(500).json({ message: SERVER_ERROR });
+	}
 };
--- apps/socket/src/index.ts
+++ apps/socket/src/index.ts
@@ -1,42 +1,42 @@
-import express from "express";
-import WebSocket, { WebSocketServer } from "ws";
-import socketManager from "./socketManager";
-import clientRouter from "./routes/orderRoutes";
 import cors from "cors";
 import "dotenv/config";
+import express from "express";
+import WebSocket, { WebSocketServer } from "ws";
+import clientRouter from "./routes/orderRoutes";
+import { addDevices, removeDevice } from "./socketManager";
 
 const app = express();
-const socketInstance = socketManager.getInstance();
+
 app.use(
-  cors({
-    origin: process.env.CLIENT_URL,
-    credentials: true,
-  })
+	cors({
+		origin: process.env.CLIENT_URL,
+		credentials: true,
+	})
 );
 
 app.get("/", () => {
-  console.log("Hello World");
+	console.log("Hello World");
 });
 
 app.use("/api/v1", clientRouter);
 
 const httpServer = app.listen(process.env.PORT, () => {
-  console.log(`Server is running on port ${process.env.PORT}`);
+	console.log(`Server is running on port ${process.env.PORT}`);
 });
 
 const wss = new WebSocketServer({ server: httpServer });
 
 wss.on("connection", (ws: WebSocket) => {
-  console.log("A user connected");
-  ws.on("message", (data: string) => {
-    const parsedData = JSON.parse(data);
-    if (parsedData.type === "init") {
-      socketInstance.addDevices(ws, parsedData.id);
-    }
-  });
+	console.log("A user connected");
+	ws.on("message", (data: string) => {
+		const parsedData = JSON.parse(data);
+		if (parsedData.type === "init") {
+			addDevices(ws, parsedData.id);
+		}
+	});
 
-  ws.on("close", () => {
-    const socket = ws;
-    socketInstance.removeDevice(socket);
-  });
+	ws.on("close", () => {
+		const socket = ws;
+		removeDevice(socket);
+	});
 });
