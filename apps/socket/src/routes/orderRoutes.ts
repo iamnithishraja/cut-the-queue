d3167488af8dee2ca759f7094c5607d7ae35b3f0
Labbey Ammar Ahmed <141624572+ammar-codeable@users.noreply.github.com>
2024-11-04 18:35:42 +0530

    feat: add route to handle order handover

--- apps/socket/src/routes/orderRoutes.ts
+++ apps/socket/src/routes/orderRoutes.ts
@@ -1,8 +1,9 @@
 import express from "express";
-import { broadcastQuantity } from "../controllers/orderControllers";
+import { broadcastQuantity, handleOrderHandover } from "../controllers/orderControllers";
 
 const clientRouter = express.Router();
 
 clientRouter.get("/notifyQuantity/:canteenId", broadcastQuantity);
+clientRouter.post("/handover/:orderId", handleOrderHandover);
 
 export default clientRouter;
