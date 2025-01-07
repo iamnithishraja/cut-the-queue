95de7eeec46cfbf187291eef90a108ce30b6632a
Nithish Raja <153272518+iamnithishraja@users.noreply.github.com>
2025-01-07 18:20:28 +0530

    feat: k8s config

---
+++ apps/tests/loadtest.js
@@ -0,0 +1,25 @@
+import http from 'k6/http';
+import { sleep } from 'k6';
+
+export const options = {
+  scenarios: {
+    spike_test: {
+      executor: 'ramping-arrival-rate',
+      startRate: 1,
+      timeUnit: '1s',
+      preAllocatedVUs: 50,
+      maxVUs: 100,
+      stages: [
+        { duration: '30s', target: 500 },   // Normal load
+        { duration: '30s', target: 5000 },  // Spike
+        { duration: '1m', target: 50 },   // Maintain spike
+        { duration: '30s', target: 5 },   // Scale down
+      ],
+    },
+  },
+};
+
+export default function () {
+  http.get('http://34.131.19.111');
+  sleep(0.1);
+}
