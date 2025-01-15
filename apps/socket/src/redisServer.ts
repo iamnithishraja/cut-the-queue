8e900c94667280d7aa70b449149e72043577e6de
Mohammed Madni <mohammedmadni09@gmail.com>
2025-01-15 13:55:31 +0530

    feat : connect subscriber and publisher clienst to redis server

--- apps/socket/src/redisServer.ts
+++ apps/socket/src/redisServer.ts
@@ -1,19 +1,31 @@
-import { createClient } from 'redis';
-export class redis{
-    private static instance:redis | null=null;
-    private publisher: ReturnType<typeof createClient>
-   private subscriber: ReturnType <typeof createClient>
-    constructor(){
-     this.publisher=createClient();
-     this.subscriber=createClient();
+import { createClient } from "redis";
+import { StateManager } from "./stateManager";
+
+export class redis {
+  private static instance: redis | null = null;
+  publisher: ReturnType<typeof createClient>;
+  private subscriber: ReturnType<typeof createClient>;
+  constructor() {
+    this.publisher = createClient({ url: process.env.REDIS_SERVER_URL });
+    this.subscriber = createClient({ url: process.env.REDIS_SERVER_URL });
+    this.connectClients();
+  }
+   static getInstance(): redis {
+    if (!redis.instance) {
+      return new redis();
+    } else {
+      return redis.instance;
     }
-    public static getInstance():redis{
-        if(!redis.instance){
-            return new redis();
-        }
-        else{
-            return redis.instance;
-        }
-    }
-    
+  } 
+  async connectClients(){
+    await this.publisher.connect();
+    await this.subscriber.connect();
+    this.subscriber.subscribe('chat',(message)=>{
+     //do computation here
+     console.log(message);
+   });
+  }
+  publish(message){
+    this.publisher.publish('chat',JSON.stringify(message))
+  }
 }
