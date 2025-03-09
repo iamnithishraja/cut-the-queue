df010b4691e7d4c358dac322d8028d75283801a3
Labbey Ammar Ahmed <141624572+ammar-codeable@users.noreply.github.com>
2025-03-10 03:51:04 +0530

    feat: remove unused files, update package configurations, replace pnpm and turbo with bun

--- apps/http/src/utils/r2.ts
+++ apps/http/src/utils/r2.ts
@@ -1,6 +1,6 @@
 import { PutObjectCommand, S3Client } from "@aws-sdk/client-s3";
 import { getSignedUrl } from "@aws-sdk/s3-request-presigner";
-import { generateRandomStringWithRandomLength } from ".";
+import { generateRandomStringWithRandomLength } from "./index.js";
 
 const s3Client = new S3Client({
 	region: "auto",
