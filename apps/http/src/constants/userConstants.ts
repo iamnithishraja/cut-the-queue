b8ad68685ce538117d49203dddb2b5b547c06312
Labbey Ammar Ahmed <141624572+ammar-codeable@users.noreply.github.com>
2025-06-19 16:10:07 +0530

    feat: add user constants and authentication utility functions

---
+++ apps/http/src/constants/userConstants.ts
@@ -0,0 +1,15 @@
+export const USER_ALREADY_EXISTS = "User already exists";
+export const USER_NOT_VERIFIED = "User not verified";
+export const INVALID_INPUT = "Invalid input";
+export const SERVER_ERROR = "Server error";
+export const INVALID_CREDENTIALS = "Invalid credentials";
+export const INVALID_GOOGLE_TOKEN = "Invalid Google token";
+export const USER_NOT_REGISTERED = "User not registered";
+export const OTP_SENT = "OTP sent successfully";
+export const INVALID_OTP = "INVALID OTP";
+export const OTP_VERIFICATION_SUCCESSFUL = "INVALID OTP";
+export const CANTEENS_NOT_FOUND = "No canteens available";
+export const DISHES_NOT_FOUND = "No dishes found";
+export const BROADCAST_QUANTITY = "Updated quantity successfully broadcasted";
+export const ORDER_HANDOVER = "Order handover successful";
+export const USER_NOT_AUTHORISED = "You Are Not Allowed To Aceess This Route";
---
+++ apps/http/src/utils/auth.ts
@@ -0,0 +1,17 @@
+import dotenv from "dotenv";
+import jwt, { JwtPayload } from "jsonwebtoken";
+dotenv.config();
+
+const JWT_SECRET = process.env.JWT_SECRET || "your-secret-key";
+
+export function generateToken(userId: string): string {
+	return jwt.sign({ userId }, JWT_SECRET, { expiresIn: "365d" });
+}
+
+export function generateOTP(): string {
+	return Math.floor(1000 + Math.random() * 9000).toString();
+}
+
+export function createVerificationMessage(otp: string): string {
+	return `Cut the Q: Verify yourself with this OTP: ${otp}. Skip the wait, join the revolution!`;
+}
